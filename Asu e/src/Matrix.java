a118e3d55027920747b2c19fbd9d43d2feaa6944
The Realist <nitin.armstrong@gmail.com>
2019-02-10 18:36:52 -0500

    f

---
+++ Asu e/src/Matrix.java
@@ -0,0 +1,52 @@
+/**
+ * @author Nitin "Like A Sweater" Armstrong
+ * @version 4.20
+ * Does basic matrix functions (hopefully)
+ */
+
+import java.util.ArrayList;
+
+public class Matrix {
+int ArrayX1;
+int ArrayY1;
+int ArrayX2;
+int ArrayY2;
+    public static void main(String[] args) {
+          
+        Matrix();
+
+    }
+
+    public Matrix(int a, int b) {
+
+    }
+
+    public set(int, int) {
+
+    }
+
+    public get(int, int) {
+
+    }
+
+    public add(Matrix, Matrix) {
+
+    }
+
+    public sub(Matrix, Matrix) {
+
+    }
+
+    public mult(Matrix, Matrix) {
+
+    }
+
+    public mult(Matrix, double) {
+
+    }
+
+    public transpose(Matrix) {
+
+    }
+
+}
