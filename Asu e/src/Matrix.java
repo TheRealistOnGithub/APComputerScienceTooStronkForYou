25a4dc18cbfdd9e132d4dfb177312cfd0edd5016
The Realist <nitin.armstrong@gmail.com>
2019-02-18 12:40:23 -0500

    did shit

--- Asu e/src/Matrix.java
+++ Asu e/src/Matrix.java
@@ -22,24 +22,35 @@ public class Matrix {
      * @param column: the number of columns in the array
      */
     public Matrix(int row, int column) {
+
         array = new double[row][column];
     }
 
     /**
      * Sets a value within a certain index of the matrix
      *
-     * @param row    the row parameter for the array
-     * @param column the column parameter of the array
-     * @param value  the value to be inserted into the array
+     * @param x     the row parameter for the array
+     * @param y     the column parameter of the array
+     * @param value the value to be inserted into the array
      * @return the matrix
      */
-    public Matrix set(int row, int column, double value) {
-
+    public Matrix set(int x, int y, double value) {
+        Matrix One = new Matrix(row, column);
+        One.array[x][y] = value;
+        return One;
 
     }
 
-    public Matrix get(int row, int column) {
-
+    /**
+     * This method gets the value in a cell of the matrix
+     *
+     * @param x The row parameter of the array
+     * @param y The column parameter of the array
+     * @return the value in a cell of the specific index of the matrix
+     */
+    public double get(int x, int y) {
+        Matrix One = new Matrix(row, column);
+        return One.array[x][y];
     }
 
     /**
@@ -62,9 +73,9 @@ public class Matrix {
     /**
      * Subtracts two Matricies and outputs the resultant, if out of range, returns Matrix exception.
      *
-     * @param One
-     * @param Two
-     * @return
+     * @param One The first matrix input
+     * @param Two The second matrix input
+     * @return the resultant matrix
      */
     public Matrix sub(Matrix One, Matrix Two) {
         Matrix Three = new Matrix(row, column);
@@ -76,20 +87,31 @@ public class Matrix {
         return Three;
     }
 
+    /**
+     * Performs Matrix multiplication with two matrices
+     *
+     * @param One The first matrix
+     * @param Two The second matrix
+     * @return the resultant matrix
+     */
     public Matrix mult(Matrix One, Matrix Two) {
         Matrix Three = new Matrix(One.row, Two.column);
-        for (int x = 0; x < Three.row; x++){
-            for (int y= 0; y < Three.column; y++){
-                for (int z = 0; z < One.row; z++
+        for (int x = 0; x < Three.row; x++) {
+            for (int y = 0; y < Three.column; y++) {
+                for (int z = 0; z < One.row; z++) {
+                    Three.array[x][y] = One.array[x][z] * Two.array[z][y];
+                }
             }
         }
+        return Three;
     }
 
     /**
      * Performs scalar multiplication with a matrix and a scalar number
-     * @param One
-     * @param amplitude
-     * @return
+     *
+     * @param One       Input matrix
+     * @param amplitude the scalar value the matrix is multiplied by
+     * @return The resultant matrix
      */
     public Matrix mult(Matrix One, double amplitude) {
         for (int x = 0; x < row; x++) {
@@ -100,8 +122,19 @@ public class Matrix {
         return One;
     }
 
-    public Matrix transpose(Matrix) {
-
+    /**
+     * Transposes the inputted matrix
+     *
+     * @param One Input matrix
+     * @return Matrix that is transposed
+     */
+    public Matrix transpose(Matrix One) {
+        for (int x = 0; x < row; x++) {
+            for (int y = 0; y < column; y++) {
+                One.array[x][y] = One.array[y][x];
+            }
+        }
+        return One;
 
     }
 
