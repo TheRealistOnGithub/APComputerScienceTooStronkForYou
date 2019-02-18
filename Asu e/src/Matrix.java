c49febc33c5c1babecee99aa65b9b84ed06b68f7
The Realist <nitin.armstrong@gmail.com>
2019-02-18 11:04:58 -0500

    E

--- Asu e/src/Matrix.java
+++ Asu e/src/Matrix.java
@@ -4,49 +4,105 @@
  * Does basic matrix functions (hopefully)
  */
 
-import java.util.ArrayList;
 
 public class Matrix {
-int ArrayX1;
-int ArrayY1;
-int ArrayX2;
-int ArrayY2;
-    public static void main(String[] args) {
-          
-        Matrix();
+    int row;
+    int column;
+    double[][] array;
+    double[][] testArray;
+
+    public static void main(String[] Args) {
 
     }
 
-    public Matrix(int a, int b) {
+    /**
+     * Creates a matrix
+     *
+     * @param row:    the number of rows in  the array
+     * @param column: the number of columns in the array
+     */
+    public Matrix(int row, int column) {
+        array = new double[row][column];
+    }
+
+    /**
+     * Sets a value within a certain index of the matrix
+     *
+     * @param row    the row parameter for the array
+     * @param column the column parameter of the array
+     * @param value  the value to be inserted into the array
+     * @return the matrix
+     */
+    public Matrix set(int row, int column, double value) {
+        array[row][column] = value;
 
     }
 
-    public set(int, int) {
+    public Matrix get(int row, int column) {
+        System.out.println(array[row][column]);
+    }
+
+    /**
+     * Adds two Matrices and outputs the resultant
+     *
+     * @param One The first matrix
+     * @param Two The second matrix
+     * @return The output matrix
+     */
+    public Matrix add(Matrix One, Matrix Two) {
+        Matrix Three = new Matrix(row, column);
+        for (int x = 0; x < row; x++) {
+            for (int y = 0; y < column; y++) {
+                Three.array[x][y] = One.array[x][y] + Two.array[x][y];
+            }
+        }
+        return Three;
+    }
+
+    public Matrix sub(Matrix One, Matrix Two) {
+        Matrix Three = new Matrix(row, column);
+        for (int x = 0; x < row; x++) {
+            for (int y = 0; y < column; y++) {
+                Three.array[x][y] = One.array[x][y] - Two.array[x][y];
+            }
+        }
+        return Three;
+    }
+
+    public Matrix mult(Matrix, Matrix) {
 
     }
 
-    public get(int, int) {
+    public Matrix mult(Matrix, double) {
 
     }
 
-    public add(Matrix, Matrix) {
+    public Matrix transpose(Matrix) {
+
 
     }
 
-    public sub(Matrix, Matrix) {
+}
 
+/**
+ * MatrixException
+ * <p>
+ * This exception is thrown for any error associated with the
+ * <p>
+ * methods in the Matrix class.
+ *
+ * @author Mr. Kramer
+ * @version 10/3/2010
+ */
+
+class MatrixException extends Exception {
+
+    public MatrixException() {
+        super();
     }
 
-    public mult(Matrix, Matrix) {
-
-    }
-
-    public mult(Matrix, double) {
-
-    }
-
-    public transpose(Matrix) {
-
+    public MatrixException(String s) {
+        super(s);
     }
 
 }
